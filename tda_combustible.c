2fbdb829e0ef0ad5541b368ea95145bee95b7fa7
NicoHoffmann99 <nhoffmann@fi.uba.ar>
2022-07-10 16:55:17 -0300

    add tda_combustible

---
+++ tda_combustible.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct combustible{
+  bool vive;
+  bool activa;
+  int combustible;
+  double posx;
+  double posy;
+  double angulo;
+};
+
+typedef struct combustible combustible_t;
+
+combustible_t *combustible_crear(double posx, double posy, double ang){
+  combustible_t *combustible=malloc(sizeof(combustible_t));
+  if(combustible==NULL){
+    return NULL;
+  }
+  combustible->vive=true;
+  combustible->activa=false;
+  combustible->combustible=3000;
+  combustible->posx=posx;
+  combustible->posy=posy;
+  combustible->angulo=ang;
+  return combustible;
+}
+
+void combustible_eliminar(combustible_t *comb){
+  free(comb);
+}
+
+double combustible_posx(combustible_t *comb){
+  return comb->posx;
+}
+
+double combustible_posy(combustible_t *comb){
+  return comb->posy;
+}
+
+double combustible_angulo(combustible_t *comb){
+  return comb->angulo;
+}
+
+void combustible_matar(combustible_t *comb){
+  comb->vive=false;
+  comb->combustible=0;
+}
