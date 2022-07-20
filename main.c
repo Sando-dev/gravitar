51772a3014b12c88aa818d220fc1ca3b0523a62d
NicoHoffmann99 <nhoffmann@fi.uba.ar>
2022-07-19 23:33:13 -0300

    torretas disparan

--- main.c
+++ main.c
@@ -115,10 +115,12 @@ int main() {
 
 
     // Queremos que todo se dibuje escalado por f:
+    float tiempo_coldown_torretas=0;
     float centro=0;
     float f = 1;
     lista_t *disparos=lista_crear();
     lista_t *torretas_lista = lista_crear();
+    lista_t *torreta_disparos= lista_crear();
     lista_t *fuel_lista = lista_crear();
     // END código del alumno
 
@@ -195,17 +197,6 @@ int main() {
           n_nave=figura_cant_polilineas(figura_vector[nave_en_vector]);
         }
 
-        /*if(nave_escudo_esta_prendido(navei)){
-          polilinea_t **escudo=copiar_polilineas(figura_vector[escudo_en_vector],nave_get_posx(navei)-centro+ VENTANA_ANCHO / 2 / f,nave_get_posy(navei),nave_get_angulo(navei)-NAVE_ANGULO_INICIAL);
-
-          for(size_t i=0; i<figura_cant_polilineas(figura_vector[escudo_en_vector]);i++){
-            graficar_polilinea(renderer,escudo[i],f);
-          }
-          destruir_vector_polilineas(escudo,figura_cant_polilineas(figura_vector[escudo_en_vector]));
-        }*/
-
-
-
 
         for(size_t i=0; i<n_nave;i++){
           graficar_polilinea(renderer, nave[i], f);
@@ -223,11 +214,12 @@ int main() {
         }
 
         if(hay_nivel_activo){
-
+          tiempo_coldown_torretas+=(float)1/JUEGO_FPS;
           float angulo_estrella=((float)3/2)*PI;
           nave_mover(navei,a,angulo,angulo_estrella);
 
           lista_iterar(disparos,disparo_mover,NULL);
+          lista_iterar(torreta_disparos,disparo_mover,NULL);
 
           size_t nivel_en_figuras = figura_buscar(figura_vector,n_figura,nivel_nombre(niveles[indice_nivel]));
 
@@ -252,6 +244,7 @@ int main() {
             disparo_t *disparo=lista_iter_ver_actual(iter_disparos);
             size_t n_disparo=figura_cant_polilineas(figura_vector[disparo_en_vector]);
             polilinea_t **disparo_polilinea=copiar_polilineas(figura_vector[disparo_en_vector],disparo_get_posx(disparo)-centro+ VENTANA_ANCHO / 2 / f,disparo_get_posy(disparo),disparo_get_angulo(disparo));
+
             for(size_t i=0; i<n_nivel;i++){
               if(distancia_punto_a_polilinea(nivel_polilinea[i],disparo_get_posx(disparo),disparo_get_posy(disparo))<=1){
                 lista_iter_borrar(iter_disparos);
@@ -259,6 +252,7 @@ int main() {
                 break;
               }
             }
+
             for(size_t i=0; i<n_disparo;i++){
               graficar_polilinea(renderer,disparo_polilinea[i],f);
             }
@@ -267,14 +261,65 @@ int main() {
             lista_iter_avanzar(iter_disparos);
           }
           lista_iter_destruir(iter_disparos);
+
+
+          lista_iter_t *iter_disparos_torretas=lista_iter_crear(torreta_disparos);
+          while(!lista_iter_al_final(iter_disparos_torretas)){
+            disparo_t *disparo=lista_iter_ver_actual(iter_disparos_torretas);
+            size_t n_disparo=figura_cant_polilineas(figura_vector[disparo_en_vector]);
+            polilinea_t **disparo_polilinea=copiar_polilineas(figura_vector[disparo_en_vector],disparo_get_posx(disparo)-centro+ VENTANA_ANCHO / 2 / f,disparo_get_posy(disparo),disparo_get_angulo(disparo));
+
+            for(size_t i=0; i<n_disparo;i++){
+              graficar_polilinea(renderer,disparo_polilinea[i],f);
+            }
+
+            if(norma(disparo_get_posx(disparo)-nave_get_posx(navei),disparo_get_posy(disparo)-nave_get_posy(navei))<=5){
+              if(nave_escudo_esta_prendido(navei)){
+                lista_iter_borrar(iter_disparos_torretas);
+                free(disparo);
+              }
+              else{
+                f = nivel_return_escala_inicial(niveles[indice_nivel]);
+                nave_matar(navei,400/f,500/f);
+                centro=nave_get_posx(navei);
+                lista_iter_borrar(iter_disparos_torretas);
+                free(disparo);
+              }
+              destruir_vector_polilineas(disparo_polilinea,n_disparo);
+              lista_iter_avanzar(iter_disparos_torretas);
+              continue;
+            }
+
+            for(size_t i=0; i<n_nivel;i++){
+              if(distancia_punto_a_polilinea(nivel_polilinea[i],disparo_get_posx(disparo),disparo_get_posy(disparo))<=0.1){
+                lista_iter_borrar(iter_disparos_torretas);
+                free(disparo);
+                break;
+              }
+            }
+
+            destruir_vector_polilineas(disparo_polilinea,n_disparo);
+            lista_iter_avanzar(iter_disparos_torretas);
+          }
+          lista_iter_destruir(iter_disparos_torretas);
+
+
+
           lista_iter_t *iter_torretas = lista_iter_crear(torretas_lista);
           while(!lista_iter_al_final(iter_torretas)){
             torreta_t *torreta = lista_iter_ver_actual(iter_torretas);
             if(torreta_vive(torreta)){
               size_t n_torreta = figura_cant_polilineas(figura_vector[torreta_en_vector]);
               polilinea_t **torreta_polilinea = copiar_polilineas(figura_vector[torreta_en_vector], torreta_get_posx(torreta)-centro+ VENTANA_ANCHO / 2 / f, torreta_get_posy(torreta), torreta_get_angulo(torreta));
+
               for(size_t i=0; i<n_torreta;i++){
                 graficar_polilinea(renderer,torreta_polilinea[i],f);
+              }
+
+              if(norma(torreta_get_posx(torreta)-nave_get_posx(navei),torreta_get_posy(torreta)-nave_get_posy(navei))<100 && tiempo_coldown_torretas>0.5){
+                float angulo_disparo=atan2(nave_get_posy(navei)-torreta_get_posy(torreta),nave_get_posx(navei)-torreta_get_posx(torreta));
+                lista_insertar_ultimo(torreta_disparos,crear_disparo(torreta_get_posx(torreta),torreta_get_posy(torreta),angulo_disparo));
+                tiempo_coldown_torretas=0;
               }
 
               lista_iter_t *iter_disparos=lista_iter_crear(disparos);
@@ -334,6 +379,8 @@ int main() {
             lista_destruir(torretas_lista,free);
             torretas_lista = lista_crear();
             lista_destruir(fuel_lista,free);
+            lista_destruir(torreta_disparos,free);
+            torreta_disparos=lista_crear();
             fuel_lista = lista_crear();
             f = 1;
           }
@@ -472,6 +519,8 @@ int main() {
               free(f);
               lista_destruir(disparos,free);
               disparos=lista_crear();
+              lista_destruir(torreta_disparos,free);
+              torreta_disparos=lista_crear();
             }
           }
 
@@ -495,7 +544,8 @@ int main() {
               centro=nave_get_posx(navei);
               lista_destruir(disparos,free);
               disparos=lista_crear();
-
+              lista_destruir(torreta_disparos,free);
+              torreta_disparos=lista_crear();
             }
           }
 
@@ -519,6 +569,8 @@ int main() {
               free(f);
               lista_destruir(disparos,free);
               disparos=lista_crear();
+              lista_destruir(torreta_disparos,free);
+              torreta_disparos=lista_crear();
             }
           }
 
@@ -542,6 +594,8 @@ int main() {
               free(f);
               lista_destruir(disparos,free);
               disparos=lista_crear();
+              lista_destruir(torreta_disparos,free);
+              torreta_disparos=lista_crear();
             }
           }
 
@@ -565,6 +619,8 @@ int main() {
               free(f);
               lista_destruir(disparos,free);
               disparos=lista_crear();
+              lista_destruir(torreta_disparos,free);
+              torreta_disparos=lista_crear();
             }
           }
 
@@ -666,6 +722,7 @@ int main() {
     encabezado_destruir(e);
 
     lista_destruir(disparos,free);
+    lista_destruir(torreta_disparos,free);
     lista_destruir(torretas_lista,free);
     lista_destruir(fuel_lista, free);
 
